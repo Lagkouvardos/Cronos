c48763456497fae1fe03c1ed10c6f8dbda8c361c
Arislitos <arislit@live.com>
2021-09-04 15:34:01 +0300

    Added meta-file and debug

--- Working_Chronos/Main_script.R
+++ Working_Chronos/Main_script.R
@@ -11,7 +11,7 @@ setwd("~/Working_Chronos/")              #<--- CHANGE ACCORDINGLY !!!
 #' Please give the file name of the normalized OTU-table without taxonomic classification
 input_otu = "OTUs-TableIS.tab"           #<--- CHANGE ACCORDINGLY !!!
 #' Please give the name of the meta-file that contains individual sample information
-input_meta = "Map_OTUsIS"                #<--- CHANGE ACCORDINGLY !!!
+input_meta = "Meta_Inf_Stud.txt"                #<--- CHANGE ACCORDINGLY !!!
 #' Please give the name of the phylogenetic tree constructed from the OTU sequences
 input_tree = "OTUs-NJTree.treIS"         #<--- CHANGE ACCORDINGLY !!!
 
@@ -28,12 +28,12 @@ adult_timepoint_name = 'MM'              #<--- CHANGE ACCORDINGLY
 # 4: Order
 # 5: Family
 
-taxonomic_level=5  #### <---- CHANGE ACCORDINGLY
+taxonomic_level=4                          # <---- CHANGE ACCORDINGLY
 
 
 # Please select method with which the optimal number of clusters will be selected
 # Could be either Drop or Highest
-clustering_method='Drop'  # <---- CHANGE ACCORDINGL
+clustering_method='Drop'                # <---- CHANGE ACCORDINGLy
 
 # Please select method to declare the transition matrix
 # It can be: mle (Maximum Likelihood Estimation), map (Maximum a posteriori) or  bootstrap
@@ -51,7 +51,7 @@ markov_method= 'mle'
 ############################ READING THE FILES ###########################################
 ##########################################################################################
 
-meta_file <- read.table (file = input_meta, check.names = FALSE, header = TRUE, dec = ".", sep = ",", row.names = 1, comment.char = "")
+meta_file <- read.table (file = input_meta, check.names = FALSE, header = TRUE, dec = ".", sep = "\t", row.names = 1, comment.char = "", stringsAsFactors = F)
 # Clean table from empty lines
 meta_file <- data.frame(meta_file[!apply(is.na(meta_file) | meta_file=="",1,all),])
 # Order the mapping file by sample names (ascending)
@@ -106,16 +106,25 @@ taxa_matrix <- data.frame(t(taxa_matrix))
 ##########################################################################################
 ################# CONVERT FILES TO DESIRABLE FORMAT ######################################
 ##########################################################################################
-
-
+kati <- c()
+kati_allo <-c()
+for (i in rownames(meta_file)){
+  kati<- append(kati, paste(ifelse(nchar(i)==5,yes = 'X',no = 'X0'), i, sep = ""))
+  kati_allo <- append(kati_allo, paste(ifelse(nchar(i)==2,yes = "X0",no = "X"),meta_file[i,'Sample'], sep = "")) 
+}
+rownames(meta_file) <- kati
+meta_file[,"Sample"]
+kati_allo
+meta_file[,'Sample'] <- kati_allo
 # keep only those rows that appear in the mapping file
 otu_file <- otu_file[,rownames(meta_file)]
-
 # OTU-table and mapping file should have the same order and number of sample names
 # Order the OTU-table by sample names (ascending)
 otu_file <- otu_file[,order(names(otu_file))]
 # Transpose OTU-table and convert format to a data frame
 otu_file <- data.frame(t(otu_file))
+# keep only those that appear in the otu file
+meta_file <- meta_file[rownames(otu_file),]
 
 ######################### END OF SECTION #################################################
 
@@ -270,7 +279,6 @@ optimal_k<- function(unifract_dist, method){
 k=optimal_k(unifract_dist = unifract_dist, method = clustering_method)
 clusters <- clustering_function(unifract_dist = unifract_dist,k = k, kentra = sample(x = row.names(unifract_dist), size = k))[[1]]
 
-
 samples_on_clusters[meta_file[row.names(unifract_dist),'Sample'],name]<- clusters
 }
 
@@ -332,9 +340,10 @@ for (name in names(transition_matrices)){
 
 ######################### END OF SECTION #################################################
 
-
+meta_file[,'Sample']
 
 ############## PRACTICE ############################
+
 
 ############## COMMENTS ############################
 # Καταλήγω με πίνακα που θα έχει τα διαφορετικά clusters ανά δείγμα και ως features ????
